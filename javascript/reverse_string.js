ed1194c42487ba2e3ad9adc42143937b85aa6a52
Nia K <nsjkappen@gmail.com>
2021-11-08 19:40:04 -0800

    push

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,19 +1,30 @@
 function reverseString(str) {
-  // type your code here
+  var reversed = "";
+  for (var i = str.length -1; i >= 0; i--){
+    reversed += str[i];
+    
+  }
+  return reversed;
+
 }
 
 if (require.main === module) {
   // add your own tests in here
-  console.log("Expecting: 'ih'");
-  console.log("=>", reverseString("hi"));
+  console.log("hello").length()
+  // console.log("Expecting: 'ih'");
+  // console.log("=>", reverseString("hi"));
 
-  console.log("");
+  // console.log("");
 
-  console.log("Expecting: 'ybabtac'");
-  console.log("=>", reverseString("catbaby"));
+  // console.log("Expecting: 'ybabtac'");
+  // console.log("=>", reverseString("catbaby"));
 }
 
 module.exports = reverseString;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+
+  // iterate through each letter and want to print the
+  // last letter first 
